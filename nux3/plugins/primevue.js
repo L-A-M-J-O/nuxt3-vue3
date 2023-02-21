bf24307009d0c74de768fe724579a858e29ef68f
Alan Naranjo <85073731+L-A-M-J-O@users.noreply.github.com>
2023-02-21 22:57:28 +0100

    push

--- nux3/plugins/primevue.js
+++ nux3/plugins/primevue.js
@@ -1,9 +1,13 @@
 import { defineNuxtPlugin } from "#app";
 import PrimeVue from "primevue/config";
 import Button from "primevue/button";
+import DataTable from 'primevue/datatable';
+import Column from 'primevue/column';
 
 export default defineNuxtPlugin((nuxtApp) => {
     nuxtApp.vueApp.use(PrimeVue, {ripple: true});
     nuxtApp.vueApp.component('Button', Button);
+    nuxtApp.vueApp.component('DataTable', DataTable);
+    nuxtApp.vueApp.component('Column', Column);
     //other components that you need
 });
