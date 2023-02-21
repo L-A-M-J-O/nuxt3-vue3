5313dcb04f2c285c5a46639ee187781bb053ba7a
Alan Naranjo <85073731+L-A-M-J-O@users.noreply.github.com>
2023-02-21 21:34:32 +0100

    nuxt3

---
+++ nux3/app.vue
@@ -0,0 +1,9 @@
+<script setup>
+</script>
+<template>
+  <div>
+    <h1>PRIME VUE</h1>
+    <Button/>
+    <NuxtWelcome />
+  </div>
+</template>
---
+++ nux3/nuxt.config.ts
@@ -0,0 +1,11 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+    css: [
+        'primevue/resources/themes/saga-blue/theme.css',
+        'primevue/resources/primevue.css',
+        'primeicons/primeicons.css'
+    ],
+	build: {
+		transpile: ['primevue']
+	}
+})
